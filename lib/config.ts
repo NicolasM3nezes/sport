525731da9f9680d6d8e8c83ff1828ae0c096e43f
Nicolas Menezes <nicolas.seabra09@gmail.com>
2026-09-03 10:55:55 -0300

    feat: add application configuration

---
+++ lib/config.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL=process.env.NEXT_PUBLIC_SUPABASE_URL||'https://lgjviajyztekqnhjxfrl.supabase.co';
+export const SUPABASE_KEY=process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY||'sb_publishable_9qCcNewrU4e1DV7dK0zm7A_BUEMUTO2';
+export const APP_TZ='America/Sao_Paulo';
