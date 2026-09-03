da8826744f7f135c4f86477013b6925077c6b319
Nicolas Menezes <nicolas.seabra09@gmail.com>
2026-09-03 10:56:02 -0300

    feat: add Supabase client

---
+++ lib/supabase.ts
@@ -0,0 +1,3 @@
+import {createClient} from '@supabase/supabase-js';
+import {SUPABASE_URL,SUPABASE_KEY} from './config';
+export const db=createClient(SUPABASE_URL,SUPABASE_KEY,{auth:{persistSession:false}});
